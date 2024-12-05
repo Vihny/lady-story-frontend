acb8caa46272ddc3f8b812a541436c95a40c039f
hiali <hialidias@gmail.com>
2024-12-04 21:03:41 -0300

    feat: adicao do carrosel

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,25 +1,31 @@
-import TotalBalance from "../total-balance/total-balance"
-import { Card, CardContent } from "../ui/card"
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "../ui/carousel"
+import './carousel.scss';
+import TotalBalance from '../total-balance/total-balance';
+import { CardContent } from '../ui/card';
+import { Carousel, CarouselContent, CarouselItem } from '../ui/carousel';
 
 export function CarouselDemo() {
   return (
-    <Carousel className="md:w-[880px] max-w-xs">
+    <Carousel className='md:w-[880px] max-w-xs'>
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="carousel-container">
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <CarouselItem className="basis-3/3"><TotalBalance /></CarouselItem>
-                </CardContent>
+          <CarouselItem
+            key={index}
+            style={{ background: '#fff', width: 600, borderRadius: 12 }}
+          >
+            <div className='carousel-container'>
+              <div className='total-card'>
+                <p>$240,399</p>
+                <span>Todas as contas</span>
+              </div>
+              <CardContent className='flex aspect-square items-center justify-center p-6'>
+                <CarouselItem className='basis-3/3'>
+                  <TotalBalance />
+                </CarouselItem>
+              </CardContent>
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
     </Carousel>
-  )
+  );
 }
